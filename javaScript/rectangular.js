1d9d626ecb7715fd62e2a198e2cd5a83c59e1305
razzak118348 <abdurrazzak118348@gmail.com>
2024-02-23 17:25:24 +0600

    triangle

---
+++ javaScript/rectangular.js
@@ -0,0 +1,23 @@
+/* 
+*objective: get base and height and calculate the area by given formula
+
+
+*/
+
+
+function claculateTriangleArea(){
+   //base
+    const triangleBaseInput=document.getElementById('triangle-base');
+    const triangleBaseText=triangleBaseInput.value ;
+     const base =parseFloat(triangleBaseText);
+
+     //height
+     const triangleHeightInput=document.getElementById('triangle-height');
+     const triangleHeightText=triangleHeightInput.value;
+     const height=parseFloat(triangleHeightText);
+
+    const area=0.5*base*height;
+    console.log(area)
+
+    
+}
